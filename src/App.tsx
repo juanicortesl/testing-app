38d8286577bee8dbeb87d1025b23e0770b91131a
juanicortesl <jicortes6@uc.cl>
2022-12-21 10:17:16 -0300

    button color test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [buttonColor, setButtonColor] = React.useState('red');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <button
+        style={{ backgroundColor: buttonColor }}
+        onClick={() => setButtonColor(buttonColor === 'red'? 'blue': 'red')}
+      >Change to {
+           buttonColor === 'red'? 'blue': 'red'}</button>
     </div>
   );
 }
